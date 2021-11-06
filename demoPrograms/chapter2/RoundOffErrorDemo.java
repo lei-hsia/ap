e0377cccb166ae3a1346dac6f283e01b69a9fdaa
Lei Xia <GenuineLei@163.com>
2021-11-06 11:03:26 +0800

    Create RoundOffErrorDemo.java

---
+++ demoPrograms/chapter2/RoundOffErrorDemo.java
@@ -0,0 +1,12 @@
+class RoundOffErrorDemo {
+	public static void main(String[] args) {
+        double a = 0.7;
+        double b = 0.9;
+        double x = a + 0.1;
+        double y = b - 0.1;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y );
+        System.out.println(x == y);
+    }
+}
